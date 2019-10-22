2994d404b7d57ed015b6a13b9f17017d6c7a1d40
Max Bothe <mathebox@gmail.com>
2019-10-22 22:17:32 +0200

    Handle multiple directories on DSLR

--- app/dslr.py
+++ app/dslr.py
@@ -1,9 +1,12 @@
 from app import DEFAULT_IMAGE_PATH
 import os
 import subprocess
+import time
 
+IMAGE_FOLDER = "100CANON"
 
 def init():
+    global IMAGE_FOLDER
     detect_proc = subprocess.Popen(['gphoto2', ' --auto-detect', '--quiet'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     _, _ = detect_proc.communicate()
     target_proc = subprocess.Popen(['gphoto2', ' --get-config', 'capturetarget'], stdout=subprocess.PIPE)
@@ -14,9 +17,16 @@ def init():
             print 'Set capture target to memory card.'
             set_proc.wait()
 
+    folder_proc = subprocess.Popen(['gphoto2', '--list-folders'], stdout=subprocess.PIPE)
+    for line in folder_proc.stdout:
+        stripped_line = line.strip()
+        if stripped_line.startswith('-') and stripped_line.endswith('CANON'):
+            IMAGE_FOLDER = line.split()[1]
+            print('Use photos for folder %s' % IMAGE_FOLDER)
+
 
 def current_photo_count():
-    ls_proc = subprocess.Popen(['gphoto2', '--num-files', '--folder=/store_00020001/DCIM/100CANON'], stdout=subprocess.PIPE)
+    ls_proc = subprocess.Popen(['gphoto2', '--num-files', '--folder=/store_00020001/DCIM/%s' % IMAGE_FOLDER], stdout=subprocess.PIPE)
     stdout =  list(ls_proc.stdout)
     if len(stdout) > 0:
         try:
@@ -33,20 +43,24 @@ def download_latest_photo(photo_nr):
         rm_proc = subprocess.Popen(['rm', default_image_path_app],
                                    stdout=subprocess.PIPE)
         return
+    t1 = time.time()
     cp_proc = subprocess.Popen(['gphoto2',
                                 '--get-file=%d' % photo_nr,
                                 '--filename=%s' % raw_default_image_path_app,
                                 '--force-overwrite'])
     cp_proc.wait()
+    t2 = time.time()
     convert_proc = subprocess.Popen(['epeg',
                                 '--max=%d' % 1000,
                                 '--quality=%d' % 50,
                                 raw_default_image_path_app,
                                 default_image_path_app])
     convert_proc.wait()
+    t3 = time.time()
+
+    print 'cp:', (t2-t1), ' ---- convert:', (t3-t2)
 
 
 def capture_photo():
     capture_proc = subprocess.Popen(['gphoto2', '--capture-image', '--keep'])
     capture_proc.wait()
-
